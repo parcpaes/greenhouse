c16bbb9a3d6d59d37a71161dd350479a40e8f604
paes marca <paesm14@gmail.com>
2019-07-19 16:27:51 -0400

    fixing test attempt 2

--- src/main/java/com/invernadero/SpringsocketApplication.java
+++ src/main/java/com/invernadero/SpringsocketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @EnableScheduling
 @SpringBootApplication
 public class SpringsocketApplication extends SpringBootServletInitializer{
