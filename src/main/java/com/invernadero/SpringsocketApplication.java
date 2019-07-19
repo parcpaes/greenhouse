987446b59b6cca1d1dfc40b7ec78e8b5d1f7f868
paes marca <paesm14@gmail.com>
2019-07-19 12:55:37 -0400

    fixing the run class

--- src/main/java/com/invernadero/SpringsocketApplication.java
+++ src/main/java/com/invernadero/SpringsocketApplication.java
@@ -2,8 +2,10 @@ package com.invernadero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 public class SpringsocketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsocketApplication.class, args);
