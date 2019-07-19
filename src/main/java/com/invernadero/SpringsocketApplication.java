9a8c2e14056a4669956973fc05a381dd883fd4d1
paes marca <paesm14@gmail.com>
2019-07-19 16:17:49 -0400

    fixing test

--- src/main/java/com/invernadero/SpringsocketApplication.java
+++ src/main/java/com/invernadero/SpringsocketApplication.java
@@ -1,12 +1,14 @@
 package com.invernadero;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
-public class SpringsocketApplication {
+@EnableScheduling
+@SpringBootApplication
+public class SpringsocketApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsocketApplication.class, args);
 	}
---
+++ src/test/java/com/parcpaes/springsocket/ApiApplicationTest.java
@@ -0,0 +1,14 @@
+package com.parcpaes.springsocket;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApiApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
