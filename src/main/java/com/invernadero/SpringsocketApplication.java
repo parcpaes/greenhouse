73350442a9baa438f66a55fbec809861d1be5df1
paes marca <paesm14@gmail.com>
2019-07-19 16:35:14 -0400

    fixing test attempt 3

--- src/main/java/com/invernadero/SpringsocketApplication.java
+++ src/main/java/com/invernadero/SpringsocketApplication.java
@@ -1,7 +1,6 @@
 package com.invernadero;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- src/test/java/com/parcpaes/springsocket/SpringsocketApplicationTests.java
+++ src/test/java/com/parcpaes/springsocket/SpringsocketApplicationTests.java
@@ -2,11 +2,15 @@ package com.parcpaes.springsocket;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.invernadero.SpringsocketApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringsocketApplication.class)
+@DataJpaTest
 public class SpringsocketApplicationTests {
 
 	@Test
